e0ce7b190da2b3354ec2141ef7bfd00c1dd0af2a
Zonylaw <sunny_law@hotmail.com>
2023-01-08 20:16:52 +0000

    example of increment and decreemtn button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// https://www.google.com/search?q=reducer+explained+youtube&oq=reducer+explained+youtube&aqs=chrome..69i57j33i10i160.7079j0j7&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:4ddc25a1,vid:kK_Wqx3RnHk
+
 import './App.css';
+import {useReducer} from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1}
+    case 'decrement':
+      return {count: state.count - 1}
+    default:
+      return state
+  }
+
+}
+
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, {count: 0});
+  
+
+  const increment = () => {
+    dispatch({type: 'increment'});
+  }
+  
+  const decrement = () => {
+    dispatch({type: 'decrement'});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <p> 
+      <button onClick={increment}>+</button>
+        {state.count}
+      <button onClick={decrement}>-</button>
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+    
     </div>
   );
 }

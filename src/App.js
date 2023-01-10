b1b8a6800576564b2b33479245fac6b328a23962
Zonylaw <sunny_law@hotmail.com>
2023-01-10 22:08:27 +0000

    add reducer set up

--- src/App.js
+++ src/App.js
@@ -3,40 +3,37 @@
 import './App.css';
 import {useReducer} from 'react';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1}
-    case 'decrement':
-      return {count: state.count - 1}
-    default:
-      return state
-  }
+const reducer = (state, action) =>{
 
 }
 
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'increment':
+//       return {count: state.count + 1}
+//     case 'decrement':
+//       return {count: state.count - 1}
+//     default:
+//       return state
+//   }
+
+// }
+
 
 function App() {
-
-  const [state, dispatch] = useReducer(reducer, {count: 0});
+  const [state, dispatch] = useReducer(reducer, {text: ''})
+ 
   
 
-  const increment = () => {
-    dispatch({type: 'increment'});
-  }
-  
-  const decrement = () => {
-    dispatch({type: 'decrement'});
-  }
+ 
   return (
     <div className="App">
 
-        <p> 
-      <button onClick={increment}>+</button>
-        {state.count}
-      <button onClick={decrement}>-</button>
-        
-        </p>
+        <form type='submit'>
+          <lable>Enter a task</lable>
+          <input placeholder="Enter task"/>
+          <button>Add</button>
+        </form>
         
     
     </div>
